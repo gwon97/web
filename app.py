4bc10d67d44e609672ef0d0de3dc20ece62c48d1
gwon97 <gwon.storage.n02@gmail.com>
2025-06-03 19:41:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+import os
+import subprocess
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return "Hello from Flask on Koyeb!"
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/whoami")
+def whoami():
+    result = subprocess.run(["whoami"], stdout=subprocess.PIPE)
+    return f"Running as: {result.stdout.decode()}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
